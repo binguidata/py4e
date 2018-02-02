7216052dc9f81f1a060b8b39e6b6a68cd3cf7a02
binguidata <binguidata@gmail.com>
2018-02-02 11:50:23 -0500

    update assignment_11_1.py

--- assignment_11_1.py
+++ assignment_11_1.py
@@ -15,3 +15,21 @@ that matched the regular expression:
 #$ python grep.py
 #Enter a regular expression: java$
 #mbox.txt had 4218 lines that matched java$
+
+import re
+
+regex = input('Enter a regular expression: ')
+if len(regex) < 1:
+    print('Please enter a valid regular expression')
+    quit()
+
+num_line = 0
+fhand = open('mbox.txt')
+for line in fhand:
+    line.rstrip()
+    list = re.findall(regex, line)
+    if len(list) != 1:
+        continue
+    num_line = num_line + 1
+
+print('mbox.txt had', num_line, 'lines that matched', regex)
